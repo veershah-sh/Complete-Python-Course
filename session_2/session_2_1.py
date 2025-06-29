7e401757f6d40cfe8587314f72b9c38c4cc9d68d
Veer Shah <42229094+veershah-sh@users.noreply.github.com>
2025-06-29 15:48:56 +0530

    Create session_2_1.py

---
+++ session_2/session_2_1.py
@@ -0,0 +1,35 @@
+"""
+doc string
+"""
+
+"""
+    Primary Datatypes
+        1. Integer
+            -> +ve, -ve, 0
+            -> 54,76,90,100, 0, -32, -54
+
+        2. Float
+            -> +ve, -ve, 0
+            -> 34.60, 90.01, 0.00, 0.30, -30.40, -33.00
+
+        3. String
+            -> A-Z, a-z, 0-9, Special Charcters(@#$%&*+-)
+            -> "", ''
+
+        4. Boolean
+            -> True, False
+
+        5. None (Null)
+"""
+
+total_marks = 340 
+per = 65.0
+password = "Abc@123"
+is_present = True
+box = None
+
+print(total_marks)
+print(per)
+print(password)
+print(is_present)
+print(box)
